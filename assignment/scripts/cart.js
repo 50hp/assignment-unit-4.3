e7f5cd88a30d187a74ae95bb4a3bc7800d31a5a8
Isaac Faulkner <isaacfaulkner1@gmail.com>
2023-04-19 14:42:14 -0500

    linked ifFull into addItem

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -4,43 +4,68 @@ console.log('***** Cart Functions *****');
 
 
 let basket = [];
-console.log(basket);
+const maxItems = 5;
+console.log(`Whats in the basket ${basket}`);
 
 //adds item to arrray basket
 function addItem( item ){
     console.log('in addItem');
-    basket.push( item )
+    if( isFull() ) {
+        return false;
+    }else {
+        basket.push( item );
+        return true;
+    }
     
-    return console.log( basket, true );
 }
 
 //loops through and lists out the items in the array basket.
 function listItems() {
+    console.log('in listItems');
     for (let i=0;i<(basket.length); i++) {
         console.log(basket[i]);
    }
-
-
 
 }
 
 //When called returns basket to an empty arry
 function empty() {
+    console.log('in empty');
     basket = [];
 
 }
+
+
+
+//Function to check the number of items in array basket
+function isFull(){
+    console.log('in isFull');
+    console.log(basket.length);
+    if (basket.length<maxItems){
+        return false;
+    }
+    else {
+        return true;
+    }
+
+}
+
+
+
+
+
+
+
 
 //Test Calls
 console.log(addItem('apple'));
 console.log(addItem('pie'));
 console.log(listItems());
-
-
-
+console.log(isFull());
+console.log(addItem());
 console.log(empty());
 
 
-console.log(basket);
 
 
 
